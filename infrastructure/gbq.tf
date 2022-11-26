1e6e02082569fe4979f9827c7bfdba0250c3fd27
Dmitry Kisler <admin@dkisler.com>
2022-11-26 16:45:19 +0100

    Add pkg.go.dev extracted data model
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- infrastructure/gbq.tf
+++ infrastructure/gbq.tf
@@ -78,3 +78,96 @@ resource "google_bigquery_table" "index_stat" {
     ])
   }
 }
+
+resource "google_bigquery_table" "pkggodev" {
+  dataset_id    = google_bigquery_dataset.raw.dataset_id
+  project       = google_bigquery_dataset.raw.project
+  table_id      = "pkggodev"
+  friendly_name = "pkggodev"
+  description   = "Data from https://pkg.go.dev/"
+
+  time_partitioning {
+    type          = "DAY"
+    expiration_ms = 0
+  }
+
+  deletion_protection = false
+
+  schema = <<EOF
+[
+  {
+    "name": "path",
+    "type": "STRING",
+    "mode": "REQUIRED",
+    "description": "The module path"
+  },
+  {
+    "name": "version",
+    "type": "STRING",
+    "mode": "REQUIRED",
+    "description": "The module version"
+  },
+  {
+    "name": "imports",
+    "type": "RECORD",
+    "mode": "REQUIRED",
+    "description": "The dependencies of the given module",
+    "fields": [
+      {
+        "name": "std",
+        "type": "STRING",
+        "mode": "REPEATED",
+        "description": "The std libraries imported as dependencies by the given module"
+      },
+      {
+        "name": "nonstd",
+        "type": "STRING",
+        "mode": "REPEATED",
+        "description": "The non-std libraries imported as dependencies by the given module"
+      }
+    ]
+  },
+  {
+    "name": "importedby",
+    "type": "STRING",
+    "mode": "REPEATED",
+    "description": "The modules which use the give on as dependency"
+  },
+  {
+    "name": "timestamp",
+    "type": "TIMESTAMP",
+    "mode": "REQUIRED",
+    "description": "Time the version was first cached by proxy.golang.org"
+  }
+]
+EOF
+}
+
+resource "google_bigquery_table" "pkggodev_remain" {
+  dataset_id    = google_bigquery_table.index.dataset_id
+  project       = google_bigquery_table.index.project
+  table_id      = "pkggodev_remain"
+  friendly_name = "pkggodev_remain"
+  description   = "Modules remaining to fetch from https://pkg.go.dev/"
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  deletion_protection = false
+
+  view {
+    use_legacy_sql = false
+
+    query = join(" ", [
+      "SELECT DISTINCT",
+      join(", ", [
+        "a.path",
+      ]),
+      "FROM `${google_bigquery_table.index.project}.${google_bigquery_table.index.dataset_id}.${google_bigquery_table.index.table_id}` AS a",
+      "LEFT JOIN `${google_bigquery_table.pkggodev.project}.${google_bigquery_table.pkggodev.dataset_id}.${google_bigquery_table.pkggodev.table_id}` AS b",
+      "USING (path, version)",
+      "WHERE a.timestamp > COALESCE(b.timestamp, '1970-01-01');"
+    ])
+  }
+}
